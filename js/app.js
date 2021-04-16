2f2bc661dfd64fcad769c245490a60ce83b29061
Mahadi <mksbd.net@gmail.com>
2021-04-17 03:11:04 +0600

    add file

--- js/app.js
+++ js/app.js
@@ -57,49 +57,31 @@ $(function () {
         });
         // Back To Top button end here
 
-        //  Smooth section scroll code start here  
-        $(".header_section .menu ul li a").on('click', function (e) {
-            e.preventDefault();
-            var target = this.hash;
-            var $target = $(target);
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, 1000, 'swing', function () {
-                // window.location.hash = target;
-            });
+     */
+     //  scroll down to hide navbar  and scroll top to show navbar
+     $(window).on("scroll", function () {
+         var scrollAmount = $(window).scrollTop();
+         if (scrollAmount > 50) {
+             $("#header_section").addClass("sticky");
+         } else {
+             $("#header_section").removeClass("sticky");
+         }
+     });
+    //  Smooth section scroll code start here  
+    $("#header_section  ul li a").on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 90
+        }, 1000, 'swing', function () {
+            // window.location.hash = target;
         });
-        //  Smooth section scroll code end here
-        $('.wrapper').pagepiling({
-            menu: null,
-            direction: 'vertical',
-            verticalCentered: true,
-            sectionsColor: [],
-            anchors: [],
-            scrollingSpeed: 700,
-            easing: 'swing linear',
-            loopBottom: false,
-            loopTop: false,
-            css3: true,
-            navigation: {
-                'textColor': '#000',
-                'bulletsColor': '#000',
-                'position': 'right',
-                'tooltips': ['d1', 'd2', 'd3']
-            },
-            normalScrollElements: null,
-            normalScrollElementTouchThreshold: 5,
-            touchSensitivity: 5,
-            keyboardScrolling: true,
-            sectionSelector: 'section',
-            animateAnchor: false,
-
-            //events
-            onLeave: function (index, nextIndex, direction) {},
-            afterLoad: function (anchorLink, index) {},
-            afterRender: function () {},
-        }); */
-
+    });
     // slick slider for banner
+    $(".project_section .project_dropdown span ").on("click", function () {
+        $(".project_section .project_dropdown ul").slideToggle();
+    });
     $('.banner_slider').slick({
         autoplay: true,
         arrows: false,
